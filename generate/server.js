94c361f0176c29195550c11fb541eaa92ced640f
Globegitter <markus.padourek@gmail.com>
2015-02-23 22:34:16 +0000

    Removed spaces.

--- generate/server.js
+++ generate/server.js
@@ -1,41 +1,28 @@
 module.exports = {
 
   friendlyName: 'Sane backend logic',
-
   description: 'Handle the addon logic for the sane backend',
-
   extendedDescription: 'This example machine is part of machinepack-boilerplate, used to introduce everyone to machines.',
 
   inputs: {
-
     name: {
-
       example: 'user',
-
       description: 'Name of the resource',
-
       required: true
     },
-
     attributes: {
-
       example: 'name:string age:number',
-
       description: 'extra attributes that will be generated',
-
       required: false
     }
-
   },
 
   defaultExit: 'success',
 
   exits: {
-
     error: {
       description: 'An unexpected error occurred.'
     },
-
     success: {
       example:  {
         numLettersInName: 4,
